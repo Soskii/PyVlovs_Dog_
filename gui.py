f4f3aa43fe5f3fe6864506ed898e0367f3b54027
Soskii <a.g.pulford@gmail.com>
2018-09-16 12:33:50 +1000

    contributed to GUI
    done most of gui, integrated it into loop

--- gui.py
+++ gui.py
@@ -4,16 +4,81 @@ import tkinter.ttk as tk
 
 class Object_Interface:
     def __init__(self, gui):
+        self.gui = gui
         self.frame = Frame(gui.object_frame_main)
-        self.left_frame = Frame(self.frame, width=)
+        self.left_frame = Frame(self.frame)
+        self.right_frame = Frame(self.frame)
 
-        self.object_type_colour = Entry()
-        self.collision_type = Entry()
-        self.object_type_is_sensor = Checkbutton()
+        self.colour_frame = Frame(self.left_frame)
+        self.type_frame = Frame(self.left_frame)
 
-        self.make_paintbrush = Button()
-        self.clear_all = Button()
+        self.object_colour_label = Label(self.colour_frame, text="Object Colour", width=14, anchor=E)
+        self.object_type_label = Label(self.type_frame, text="Object Type", width=14, anchor=E)
 
+        self.object_colour = Entry(self.colour_frame)
+        self.collision_type = Entry(self.type_frame)
+        self.is_sensor = BooleanVar()
+        self.object_type_is_sensor = Checkbutton(self.left_frame, text="Collisions", var=self.is_sensor)
+
+        self.make_paintbrush = Button(self.right_frame, text="Add", width=5)
+        self.clear_all = Button(self.right_frame, text="Clear", width=5)
+
+    def pack_all(self):
+        self.gui.object_frame_main.create_window(2, (self.gui.objects.index(self)) * 95, width=379, height=93,
+                                                 window=self.frame, anchor=NW)
+        self.left_frame.pack(side=LEFT)
+        self.right_frame.pack(side=LEFT, padx=10)
+
+        self.colour_frame.pack(pady=2)
+        self.type_frame.pack(pady=2)
+
+        self.object_colour_label.pack(side=LEFT)
+        self.object_type_label.pack(side=LEFT)
+
+        self.object_colour.pack()
+        self.collision_type.pack()
+        self.object_type_is_sensor.pack(side=TOP, fill=X)
+
+        self.make_paintbrush.pack(side=LEFT)
+        self.clear_all.pack(side=LEFT)
+
+
+class Rule_Interface:
+    def __init__(self, gui):
+        self.gui = gui
+        self.frame = Frame(gui.rules_frame_main)
+        self.entries_frame = Frame(self.frame)
+        self.radio_frame = Frame(self.frame)
+
+        self.object1_frame = Frame(self.entries_frame)
+        self.object2_frame = Frame(self.entries_frame)
+
+        self.object1_label = Label(self.object1_frame, text="Object 1", width=14, anchor=E)
+        self.object2_label = Label(self.object2_frame, text="Object 2", width=14, anchor=E)
+
+        self.object_1 = Entry(self.object1_frame)
+        self.object_2 = Entry(self.object2_frame)
+
+        self.output = IntVar()
+        self.output.set(0)
+
+        self.is_punish = Radiobutton(self.radio_frame, text="Punish", indicatoron=0, width=10, variable=self.output, value=0)
+        self.is_reward = Radiobutton(self.radio_frame, text="Reward", indicatoron=0, width=10, variable=self.output, value=1)
+
+    def pack_all(self):
+        self.gui.rules_frame_main.create_window(2, (self.gui.rules.index(self)) * 95, width=379, height=93,
+                                                window=self.frame, anchor=NW)
+        self.entries_frame.pack(side=LEFT)
+        self.object1_frame.pack()
+        self.object2_frame.pack()
+        self.object1_label.pack(side=LEFT)
+        self.object2_label.pack(side=LEFT)
+        self.object_1.pack(side=LEFT)
+        self.object_2.pack(side=LEFT)
+
+        self.radio_frame.pack(pady=20)
+        self.is_punish.pack()
+        self.is_reward.pack()
 
 
 class GUI_Window:
@@ -42,10 +107,12 @@ class GUI_Window:
         """
         the rules bit
         """
+        self.rules = []
+
         self.rules_frame_header = Frame(self.rule_frame, width=400, height=30)
         self.rules_frame_main = Canvas(self.rule_frame, width=383, height=570, bg="#999999")
-        self.add_rule_button = Button(self.rules_frame_header, text="New Rule")
-        self.delete_rule_button = Button(self.rules_frame_header, text="Delete Rule")
+        self.add_rule_button = Button(self.rules_frame_header, text="New Rule", command=self.add_rule)
+        self.delete_rule_button = Button(self.rules_frame_header, text="Delete Rule", command=self.remove_rule)
         self.rules_frame_scrollbar = Scrollbar(self.rule_frame)
 
         self.rules_frame_header.pack(fill=X)
@@ -60,13 +127,13 @@ class GUI_Window:
         the object bit
         """
 
-        objects = []
+        self.objects = []
 
         self.object_frame_header = Frame(self.object_frame, width=400, height=30)
         self.object_frame_main = Canvas(self.object_frame, width=383, height=570, bg="#999999",
                                         scrollregion=(0, 0, 383, 1000))
-        self.new_object_button = Button(self.object_frame_header, text="New Object")
-        self.remove_object_button = Button(self.object_frame_header, text="Delete Object")
+        self.new_object_button = Button(self.object_frame_header, text="New Object", command=self.add_object)
+        self.remove_object_button = Button(self.object_frame_header, text="Delete Object", command=self.remove_object)
         self.object_frame_scrollbar = Scrollbar(self.object_frame)
 
         self.object_frame_main.pack_propagate(0)
@@ -85,6 +152,26 @@ class GUI_Window:
         self.simulation_speed = Scale(self.settings_frame)
 
     def add_object(self):
+        new_object_window = Object_Interface(self)
+        self.objects.append(new_object_window)
+        new_object_window.pack_all()
+
+    def remove_object(self):
+        del (self.objects[-1])
+        self.object_frame_main.delete(ALL)
+        for object in self.objects:
+            object.pack_all()
+
+    def add_rule(self):
+        new_rule_window = Rule_Interface(self)
+        self.rules.append(new_rule_window)
+        new_rule_window.pack_all()
+
+    def remove_rule(self):
+        del (self.rules[-1])
+        self.rules_frame_main.delete(ALL)
+        for rule in self.rules:
+            rule.pack_all()
 
 
 # todo
--- loop.py
+++ loop.py
@@ -1,37 +1,42 @@
 import simulation as sm
 import gui
 
-black = [0, 0, 0]
-brown = [51, 51, 0]
-yellow = [255, 255, 0]
-red = [90, 0, 0]
-
-sim = sm.Simulation()
-
-light = sm.Object_Type(yellow, True, 1)
-sim.add_static_body(light, 50, (640, 200))
-sim.add_static_body(light, 50, (40, 40))
-
-brick = sm.Object_Type(red, False, 4)
-sim.add_static_body(brick, 75, (640, 300))
-
-chassis = sm.Dog_Part([(0, 0), (50, 0), (50, 100), (0, 100)], (-25, -25), False, 0, brown)
-
-left_wheel = sm.Dog_Part([(0, 0), (10, 0), (10, 30), (0, 30)], (-36, -20), False, 0, black, wheel="L")
-right_wheel = sm.Dog_Part([(0, 0), (10, 0), (10, 30), (0, 30)], (26, -20), False, 0, black, wheel="R")
-b_left_wheel = sm.Dog_Part([(0, 0), (10, 0), (10, 30), (0, 30)], (-36, 35), False, 0, black, wheel="L")
-b_right_wheel = sm.Dog_Part([(0, 0), (10, 0), (10, 30), (0, 30)], (26, 35), False, 0, black, wheel="R")
-
-ldr = sm.Dog_Part([(0, 0), (50, 0), (50, 6), (0, 6)], (-25, -35), True, 2, yellow)
-ultra_sonic = sm.Dog_Part([(0, 0), (6, 0), (6, 40), (0, 40)], (-3, -65), True, 3, black)
-
-dog_parts = [chassis, left_wheel, right_wheel, b_left_wheel, b_right_wheel, ldr, ultra_sonic]
-sim.add_dog(dog_parts)
-
-sim.add_collision_handler(2, 1, "punish")
-sim.add_collision_handler(3, 4, "reward")
+# black = [0, 0, 0]
+# brown = [51, 51, 0]
+# yellow = [255, 255, 0]
+# red = [90, 0, 0]
+#
+# sim = sm.Simulation()
+#
+# light = sm.Object_Type(yellow, True, 1)
+# sim.add_static_body(light, 50, (640, 200))
+# sim.add_static_body(light, 50, (40, 40))
+#
+# brick = sm.Object_Type(red, False, 4)
+# sim.add_static_body(brick, 75, (640, 300))
+#
+# chassis = sm.Dog_Part([(0, 0), (50, 0), (50, 100), (0, 100)], (-25, -25), False, 0, brown)
+#
+# left_wheel = sm.Dog_Part([(0, 0), (10, 0), (10, 30), (0, 30)], (-36, -20), False, 0, black, wheel="L")
+# right_wheel = sm.Dog_Part([(0, 0), (10, 0), (10, 30), (0, 30)], (26, -20), False, 0, black, wheel="R")
+# b_left_wheel = sm.Dog_Part([(0, 0), (10, 0), (10, 30), (0, 30)], (-36, 35), False, 0, black, wheel="L")
+# b_right_wheel = sm.Dog_Part([(0, 0), (10, 0), (10, 30), (0, 30)], (26, 35), False, 0, black, wheel="R")
+#
+# ldr = sm.Dog_Part([(0, 0), (50, 0), (50, 6), (0, 6)], (-25, -35), True, 2, yellow)
+# ultra_sonic = sm.Dog_Part([(0, 0), (6, 0), (6, 40), (0, 40)], (-3, -65), True, 3, black)
+#
+# dog_parts = [chassis, left_wheel, right_wheel, b_left_wheel, b_right_wheel, ldr, ultra_sonic]
+# sim.add_dog(dog_parts)
+#
+# sim.add_collision_handler(2, 1, "punish")
+# sim.add_collision_handler(3, 4, "reward")
 
 while True:
-    wheel_values = sim.input_network()
-    sim.step(0.01, wheel_values[0], wheel_values[1])
-    sim.display_update()
+    window = gui.GUI_Window()
+    while True:
+        window.root.update_idletasks()
+        window.root.update()
+    while True:
+        wheel_values = sim.input_network()
+        sim.step(0.01, wheel_values[0], wheel_values[1])
+        sim.display_update()
